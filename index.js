708807b1f34763d7c7b22eda3e62800e841687e9
FrankBonanno <fb0023@mix.wvu.edu>
2025-05-15 17:26:42 -0400

    mobile nav toggle functionality and logo sizing

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const nav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
 // Mobile Nav Toggle
+navToggle.addEventListener('click', () => {
+	const visibility = nav.getAttribute('data-visible');
+
+	if (visibility === 'false') {
+		nav.setAttribute('data-visible', true);
+		navToggle.setAttribute('aria-expanded', true);
+	} else {
+		nav.setAttribute('data-visible', false);
+		navToggle.setAttribute('aria-expanded', false);
+	}
+});
